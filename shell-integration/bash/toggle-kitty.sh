8669e770e6f5448e559baf230f70c437b704608c
khaotik <aruhanb@gmail.com>
2023-01-22 19:58:19 +0700

    add help script for toggling quake mode

---
+++ shell-integration/bash/toggle-kitty.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -S /tmp/kitty-u${UID} ]; then
+  kitty @ --to unix:/tmp/kitty-u${UID} resize-os-window --action toggle-minimized
+else
+  kitty --class kitty --start-as fullscreen --listen-on=unix:/tmp/kitty-u${UID} -1 --detach
+fi
